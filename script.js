d58fc265ed1ccf8152a491ab794c1db0c237114b
Akhmadillo Mamirov <105142060+akhmadmamirov@users.noreply.github.com>
2023-01-06 12:11:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
     "apiKey": "aceed9c713d93225d01cc7f2258765cc",
     fetchWeather: function(city) {
         fetch(
-            "http://api.openweathermap.org/data/2.5/weather?q=" 
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
             + "&units=metric&appid=" 
             + this.apiKey
@@ -16,13 +16,13 @@ let weather = {
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
         document.querySelector(".city").innerText = "Weather in " + name; 
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h"; 
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('http://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     search: function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
